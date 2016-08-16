b82ddfcdbf58c348a8454413e8c55a292784c465
Dave Lively <davelively14@gmail.com>
2016-08-16 14:30:56 -0400

    Fixed resize top-bg-container size

--- web/static/js/sticky_menu.js
+++ web/static/js/sticky_menu.js
@@ -20,15 +20,17 @@ let StickyMenu = {
     })
 
     function addClass(el, className) {
-      if (el.classList)
-      el.classList.add(className)
-      else if (!hasClass(el, className)) el.className += " " + className
+      if (el.classList) {
+        el.classList.add(className)
+      } else if (!hasClass(el, className)) {
+        el.className += " " + className
+      }
     }
 
     function removeClass(el, className) {
-      if (el.classList)
-      el.classList.remove(className)
-      else if (hasClass(el, className)) {
+      if (el.classList) {
+        el.classList.remove(className)
+      } else if (hasClass(el, className)) {
         var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
         el.className=el.className.replace(reg, ' ')
       }
