575cdb7af5069dde7165ab4918047e0989bdd47a
Dave Lively <davelively14@gmail.com>
2016-08-15 21:57:47 -0400

    Added sticky menu in spaghetti JS

--- priv/static/js/application.js
+++ priv/static/js/application.js
@@ -45,7 +45,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(1);
-	module.exports = __webpack_require__(4);
+	module.exports = __webpack_require__(5);
 
 
 /***/ },
@@ -57,10 +57,40 @@
 /***/ },
 /* 2 */,
 /* 3 */,
-/* 4 */
+/* 4 */,
+/* 5 */
 /***/ function(module, exports) {
 
 	"use strict";
+
+	var menu_start = document.getElementById("menu"),
+	    menu_stick = "submenu-stick",
+	    main_stick = "main-stick",
+	    top_bg = document.getElementsByClassName("top-bg")[0],
+	    main = document.getElementById("main"),
+	    top_style = window.getComputedStyle(top_bg),
+	    header = parseInt(top_style.getPropertyValue('height'));
+
+	window.addEventListener("scroll", function (e) {
+	  if (window.scrollY > header) {
+	    addClass(menu_start, menu_stick);
+	    addClass(main, main_stick);
+	  } else {
+	    removeClass(menu_start, menu_stick);
+	    removeClass(main, main_stick);
+	  }
+	});
+
+	function addClass(el, className) {
+	  if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
+	}
+
+	function removeClass(el, className) {
+	  if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
+	    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+	    el.className = el.className.replace(reg, ' ');
+	  }
+	}
 
 /***/ }
 /******/ ]);
---
+++ web/static/js/application.js
@@ -0,0 +1,32 @@
+var menu_start = document.getElementById("menu"),
+    menu_stick = "submenu-stick",
+    main_stick = "main-stick",
+    top_bg = document.getElementsByClassName("top-bg")[0],
+    main = document.getElementById("main"),
+    top_style = window.getComputedStyle(top_bg),
+    header = parseInt(top_style.getPropertyValue('height'))
+
+window.addEventListener("scroll", function(e) {
+  if( window.scrollY > header) {
+    addClass(menu_start, menu_stick)
+    addClass(main, main_stick)
+  } else {
+    removeClass(menu_start, menu_stick)
+    removeClass(main, main_stick)
+  }
+})
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
