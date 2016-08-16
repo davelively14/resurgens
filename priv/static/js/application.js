2dc865fb1bdc9327b12e1844ae95c2d153153641
Dave Lively <davelively14@gmail.com>
2016-08-16 14:11:28 -0400

    Put StickyMenu in its own module

--- priv/static/js/application.js
+++ priv/static/js/application.js
@@ -59,38 +59,94 @@
 /* 3 */,
 /* 4 */,
 /* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var _sticky_menu = __webpack_require__(6);
+
+	var _sticky_menu2 = _interopRequireDefault(_sticky_menu);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	_sticky_menu2.default.onReady();
+
+	// var menu_start = document.getElementById("menu"),
+	//     menu_stick = "submenu-stick",
+	//     main_stick = "main-stick",
+	//     header = document.getElementsByClassName("top-bg-container")[0],
+	//     main = document.getElementById("main"),
+	//     top_style = window.getComputedStyle(header),
+	//     header = parseInt(top_style.getPropertyValue('height'))
+	//
+	// window.addEventListener("scroll", function(e) {
+	//   if( window.scrollY > header) {
+	//     addClass(menu_start, menu_stick)
+	//     addClass(main, main_stick)
+	//   } else {
+	//     removeClass(menu_start, menu_stick)
+	//     removeClass(main, main_stick)
+	//   }
+	// })
+	//
+	// function addClass(el, className) {
+	//   if (el.classList)
+	//     el.classList.add(className)
+	//   else if (!hasClass(el, className)) el.className += " " + className
+	// }
+	//
+	// function removeClass(el, className) {
+	//   if (el.classList)
+	//     el.classList.remove(className)
+	//   else if (hasClass(el, className)) {
+	//     var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+	//     el.className=el.className.replace(reg, ' ')
+	//   }
+	// }
+
+/***/ },
+/* 6 */
 /***/ function(module, exports) {
 
 	"use strict";
 
-	var menu_start = document.getElementById("menu"),
-	    menu_stick = "submenu-stick",
-	    main_stick = "main-stick",
-	    header = document.getElementsByClassName("top-bg-container")[0],
-	    main = document.getElementById("main"),
-	    top_style = window.getComputedStyle(header),
-	    header = parseInt(top_style.getPropertyValue('height'));
-
-	window.addEventListener("scroll", function (e) {
-	  if (window.scrollY > header) {
-	    addClass(menu_start, menu_stick);
-	    addClass(main, main_stick);
-	  } else {
-	    removeClass(menu_start, menu_stick);
-	    removeClass(main, main_stick);
-	  }
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
 	});
+	var StickyMenu = {
+	  onReady: function onReady() {
+	    window.addEventListener("scroll", function (e) {
+	      var menu_start = document.getElementById("menu"),
+	          menu_stick = "submenu-stick",
+	          main_stick = "main-stick",
+	          header = document.getElementsByClassName("top-bg-container")[0],
+	          main = document.getElementById("main"),
+	          top_style = window.getComputedStyle(header),
+	          header = parseInt(top_style.getPropertyValue('height'));
 
-	function addClass(el, className) {
-	  if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
-	}
+	      if (window.scrollY > header) {
+	        addClass(menu_start, menu_stick);
+	        addClass(main, main_stick);
+	      } else {
+	        removeClass(menu_start, menu_stick);
+	        removeClass(main, main_stick);
+	      }
+	    });
 
-	function removeClass(el, className) {
-	  if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
-	    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-	    el.className = el.className.replace(reg, ' ');
+	    function addClass(el, className) {
+	      if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
+	    }
+
+	    function removeClass(el, className) {
+	      if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
+	        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+	        el.className = el.className.replace(reg, ' ');
+	      }
+	    }
 	  }
-	}
+	};
+
+	exports.default = StickyMenu;
 
 /***/ }
 /******/ ]);
---
+++ web/static/js/sticky_menu.js
@@ -0,0 +1,40 @@
+let StickyMenu = {
+
+  onReady() {
+    window.addEventListener("scroll", function(e) {
+      var menu_start = document.getElementById("menu"),
+      menu_stick = "submenu-stick",
+      main_stick = "main-stick",
+      header = document.getElementsByClassName("top-bg-container")[0],
+      main = document.getElementById("main"),
+      top_style = window.getComputedStyle(header),
+      header = parseInt(top_style.getPropertyValue('height'))
+
+      if( window.scrollY > header) {
+        addClass(menu_start, menu_stick)
+        addClass(main, main_stick)
+      } else {
+        removeClass(menu_start, menu_stick)
+        removeClass(main, main_stick)
+      }
+    })
+
+    function addClass(el, className) {
+      if (el.classList)
+      el.classList.add(className)
+      else if (!hasClass(el, className)) el.className += " " + className
+    }
+
+    function removeClass(el, className) {
+      if (el.classList)
+      el.classList.remove(className)
+      else if (hasClass(el, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+        el.className=el.className.replace(reg, ' ')
+      }
+    }
+
+  }
+}
+
+export default StickyMenu
